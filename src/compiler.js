6437fd984eb099045c0707129159eac9d4d3e042
Mofiqul Islam <mofi0islam@gmail.com>
2018-08-27 00:52:22 +0530

    Added new tags (#16)
    * Added new tag
    
    * Update compiler.js

--- src/compiler.js
+++ src/compiler.js
@@ -135,6 +135,79 @@ function parse(cv) {
      return output;
      break;
     }
+  // a 
+  case "a":
+   switch(v[1]) {
+    case undefined:
+    case null:
+     output = "<a>" + c[1] + "</a>"
+     return output;
+     break;
+    default:
+     output = "<a " + v[1] + ">" + c[1] + "</a>"
+     return output;
+     break;
+    }
+  // Button
+  case "button":
+   switch(v[1]) {
+    case undefined:
+    case null:
+     output = "<button>" + c[1] + "</button>"
+     return output;
+     break;
+    default:
+     output = "<button " + v[1] + ">" + c[1] + "</button>"
+     return output;
+     break;
+    }
+  // Input
+  case "input":
+   switch(v[1]) {
+    case undefined:
+    case null:
+     output = "<input/>"
+     return output;
+     break;
+    default:
+     output = "<input " + v[1] + "/>"
+     return output;
+     break;
+    }
+  // Textarea
+  case "textarea":
+   switch(v[1]) {
+    case undefined:
+    case null:
+     output = "<textarea>" + c[1] + "</textarea>"
+     return output;
+     break;
+    default:
+     switch(c[1]) {
+      case null:
+      case undefined:
+       output = "<textarea " + v[1] + "></textarea>"
+       return output;
+       break;
+      default:
+       output = "<textarea " + v[1] + ">" + c[1] + "</textarea>"
+       return output;
+       break;
+     }
+    }
+  // Abbr
+  case "abbr":
+   switch(v[1]) {
+    case undefined:
+    case null:
+     output = "<abbr>" + c[1] + "</abbr>"
+     return output;
+     break;
+    default:
+     output = "<abbr " + v[1] + ">" + c[1] + "</abbr>"
+     return output;
+     break;
+    }
    // Linebreak
    case "/br/":
     output = "<br>"
