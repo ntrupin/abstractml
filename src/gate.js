3ee13677f195ff0e1afb066549ccf5ce0ffc0341
Noah Trupin <natrupin@gmail.com>
2018-10-14 14:50:31 -0400

    Update gate.js

--- src/gate.js
+++ src/gate.js
@@ -1,17 +1,24 @@
 function build(filename) {
- var xmlhttp;
- xmlhttp=new XMLHttpRequest();
- xmlhttp.open('GET', "./" + filename + ".osls", false);
- xmlhttp.send();
- x = xmlhttp.responseText
- y = x.split("\n")
- it = "";
- y.forEach(function(ele) {
-  output = parse(ele)
-  console.log(output)
-  it = it + output
-  console.log(it)
-  document.getElementById("line").innerHTML = it
-console.log(document.getElementById("whole").innerHTML)
- });
+ var getPage = new Request(filename + '.osls');
+  fetch(getPage).then(function(response) {
+    return response.text().then(function(text) {
+      y = text.split("\n")
+      it = "";
+      y.forEach(function(ele) {
+        output = parse(ele)
+        console.log(output)
+        it = it + output
+        console.log(it)
+        document.getElementById("line").innerHTML = it
+        console.log(document.getElementById("whole").innerHTML)
+      });
+    });
+  });
 }
+
+// THIS IS THE OLD METHOD OF FETCHING THE OSLS FILE, KEPT HERE FOR REFERENCE
+/* var xmlhttp;
+xmlhttp=new XMLHttpRequest();
+xmlhttp.open('GET', "./" + filename + ".osls", false);
+xmlhttp.send();
+x = xmlhttp.responseText */
