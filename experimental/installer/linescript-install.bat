84c962b5438f2f277eae9293954dec7c736a1d5d
Noah Trupin <natrupin@gmail.com>
2018-10-19 12:26:29 -0400

    Create linescript-install.bat

---
+++ experimental/installer/linescript-install.bat
@@ -0,0 +1,3 @@
+curl -o C:\Users\%USERNAME%\Desktop\compiler.js https://ntrupin.github.com/linescript/src/compiler.js
+curl -o C:\Users\%USERNAME%\Desktop\gate.js https://ntrupin.github.com/linescript/src/gate.js
+curl -o C:\Users\%USERNAME%\Desktop\base.html https://ntrupin.github.com/linescript/src/base.html
