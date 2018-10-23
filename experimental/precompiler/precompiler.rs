d345679fc7821b12b9cbbaa84675176725697f88
Noah Trupin <natrupin@gmail.com>
2018-10-23 12:34:10 -0400

    Update precompiler.rs

--- experimental/precompiler/precompiler.rs
+++ experimental/precompiler/precompiler.rs
@@ -1,13 +1,24 @@
 fn main() {
-    let split = "h1 -> style='color:blue' -> Hi!".split("-");
-    let mut count = 0;
-    for s in split {
-        match iden {
-            => print!("<{}", s),
-            1 => print!(" {}>", s),
-            2 => print!("{}", s),
-            _ => print!("-ERROR-"),
+    let split = "h1 -> style='color:blue' -> Hi!\np -> style='color:blue' -> Hi!".split("\n");
+    for spli in split {
+        let spl = spli.split(" -> ");
+        let mut count = 0;
+        for s in spl {
+            match count {
+                0 => {
+                    print!("<{} ", s)
+                },
+                1 => {
+                    print!("{}>", s)
+                },
+                2 => {
+                    print!("{}", s);
+                    print!("</{}>", "work-in-progress");
+                    count = 0;
+                },
+                _ => print!("-ERROR-"),
+            }
+            count = count + 1;
         }
-        count = count + 1;
     }
 }
