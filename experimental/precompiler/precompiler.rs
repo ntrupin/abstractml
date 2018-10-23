42cf4abf37a6819595f776ed2596a8c8e4d65824
Noah Trupin <natrupin@gmail.com>
2018-10-22 21:56:14 -0400

    Create precompiler.rs

---
+++ experimental/precompiler/precompiler.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let split = "h1 -> style='color:blue' -> Hi!".split("-");
+    let mut count = 0;
+    for s in split {
+        match iden {
+            => print!("<{}", s),
+            1 => print!(" {}>", s),
+            2 => print!("{}", s),
+            _ => print!("-ERROR-"),
+        }
+        count = count + 1;
+    }
+}
