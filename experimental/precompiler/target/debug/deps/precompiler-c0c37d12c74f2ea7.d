71b8777021ba0f38af2800429ffdb99abb801a5c
ntrooper5 <ntrupin@icloud.com>
2018-10-24 07:19:55 -0400

    Made changes

---
+++ experimental/precompiler/target/debug/deps/precompiler-c0c37d12c74f2ea7.d
@@ -0,0 +1,5 @@
+C:\Users\ntrup\projects\precompiler\target\debug\deps\precompiler-c0c37d12c74f2ea7.exe: src\main.rs
+
+C:\Users\ntrup\projects\precompiler\target\debug\deps\precompiler-c0c37d12c74f2ea7.d: src\main.rs
+
+src\main.rs:
---
+++ experimental/precompiler/target/debug/precompiler.d
@@ -0,0 +1,1 @@
+C:\Users\ntrup\projects\precompiler\target\debug\precompiler.pdb: C:\Users\ntrup\projects\precompiler\src\main.rs
