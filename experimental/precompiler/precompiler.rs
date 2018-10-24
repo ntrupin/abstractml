d31dde47e152912fa17b0612cd9684929d12b0b4
Noah Trupin <natrupin@gmail.com>
2018-10-23 21:30:23 -0400

    Update precompiler.rs

--- experimental/precompiler/precompiler.rs
+++ experimental/precompiler/precompiler.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let parts: Vec<&str> = "h1 -> style='color:blue' -> Hi!\np -> Hi!\nHi!\nh1 -> style='color:blue' -> noend"
+    let parts: Vec<&str> = "h1 -> style='color:blue' -> Hi!\np -> Hi!\nHi!\nh1 -> style='color:blue' -> noend\ndiv -> id='div'\ndiv -> style='div' -> end"
         .trim()
         .split("\n")
         .collect();
@@ -17,12 +17,18 @@ fn main() {
                         _ => println!("<{} {}>{}</{}>", tag, args, inner, tag)
                     }
                 },
+                "div"|"span"|"center"|"header"|"nav"|"main"|"form"|"table"|"th"|"tr"|"td" =>
+                    match inner {
+                        "end"|"."|" " => println!("<{} {}></{}>", tag, args, tag),
+                        _ => println!("<{} {}>{}", tag, args, inner)
+                    }
                 _ => println!("{}", tag)
             } 
         } if seg.len() == 2 {
             let (tag, args) = (seg[0], seg[1]);
             match tag {
                 "h1"|"h2"|"h3"|"h4"|"h5"|"h6"|"p"|"a"|"abbr"|"button"|"li" => println!("<{}>{}</{}>", tag, args, tag),
+                "div"|"span"|"center"|"header"|"nav"|"main"|"form"|"table"|"th"|"tr"|"td" => println!("<{} {}>", tag, args),
                 _ => println!("{}", tag)
             } 
         }
