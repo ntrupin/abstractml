1c3a20bd89e1932870c75849f65e9a989d6faeee
Noah Trupin <natrupin@gmail.com>
2018-10-26 21:28:49 -0400

    Update precompiler.rs

--- experimental/precompiler.rs
+++ experimental/precompiler.rs
@@ -1,5 +1,9 @@
-fn main() {
-    let parts: Vec<&str> = "charset -> utf-8\nmeta -> viewport -> width=device-width, initial-scale=1\nlink -> stylesheet -> index.css\ntitle -> Hello, World!\ndiv\nh1 -> Example Domain\np -> This domain is established to be used for illustrative examples in documents. You may use this domain in examples without prior coordination or asking for permission.\na -> href='http://www.iana.org/domains/example' -> More information...\nend -> div"
+extern crate autopilot;
+
+use std::env;
+
+fn compile() {
+    let parts: Vec<&str> = "charset -> utf-8\nmeta -> viewport -> width=device-width, initial-scale=1\nlink -> stylesheet -> index.css\ntitle -> Hello, World!\ndiv\nh1 -> Example Domain\np -> This domain is established to be used for illustrative examples in documents. You may use this domain in examples without prior coordination or asking for permission.\na -> href='http://www.iana.org/domains/example' -> More information...\nend -> div\nimg -> https://google.com\nimg -> https://google.com -> width='90px' height='90px'\nol\nli -> Hi!\nend -> ol\nul -> style='color:blue'"
         .trim()
         .split("\n")
         .collect();
@@ -17,11 +21,13 @@ fn main() {
                         _ => println!("<{} {}>{}</{}>", tag, args, inner, tag)
                     }
                 },
-                "div"|"span"|"center"|"header"|"nav"|"main"|"form"|"table"|"th"|"tr"|"td" =>
+                "div"|"span"|"center"|"header"|"nav"|"main"|"form"|"table"|"th"|"tr"|"td" => {
                     match inner {
                         "end"|"."|" " => println!("<{} {}></{}>", tag, args, tag),
                         _ => println!("<{} {}>{}", tag, args, inner)
                     }
+                },
+                "img" => println!("<{} src='{}' {} />", tag, args, inner),
                 _ => println!("{}", tag)
             }
         } if seg.len() == 2 {
@@ -30,14 +36,31 @@ fn main() {
                 "h1"|"h2"|"h3"|"h4"|"h5"|"h6"|"p"|"a"|"abbr"|"button"|"li" => println!("<{}>{}</{}>", tag, args, tag),
                 "div"|"span"|"center"|"header"|"nav"|"main"|"form"|"table"|"th"|"tr"|"td" => println!("<{} {}>", tag, args),
                 "end" => println!("</{}>", args),
+                "img" => println!("<{} src='{}' />", tag, args),
+                "ol"|"ul" => println!("<{} {}>", tag, args),
                 _ => println!("{}", tag)
             }
-        } else {
+        } if seg.len() == 1 {
             let tag = seg[0];
             match tag {
-                "h1"|"h2"|"h3"|"h4"|"h5"|"h6"|"p"|"a"|"abbr"|"button"|"li"|"div"|"span"|"center"|"header"|"nav"|"main"|"form"|"table"|"th"|"tr"|"td" => println!("<{}>", tag),
+                "h1"|"h2"|"h3"|"h4"|"h5"|"h6"|"p"|"a"|"abbr"|"button"|"li"|"div"|"span"|"center"|"header"|"nav"|"main"|"form"|"table"|"th"|"tr"|"td"|"img"|"ol"|"ul" => println!("<{}>", tag),
                 _ => println!("{}", tag)
             }
         }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("File to compile: {:?}", args[1]);
+    let response = autopilot::alert::alert(
+        "Welcome to the LineScript Precompiler! This is the official utility to compile LineScript into working HTML5 straight from your desktop, before you deploy it on the web.",
+        Some("LineScript Precompiler"),
+        Some("Compile"),
+        Some("Cancel"),
+    );
+    match response {
+        autopilot::alert::Response::Default => compile(),
+        autopilot::alert::Response::Cancel => println!("The process was successfully terminated."),
     }
 }
