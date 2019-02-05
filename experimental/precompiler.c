130455c6e4a5386cac2c4da39319274964ae862f
Noah Trupin <natrupin@gmail.com>
2019-02-05 16:11:13 -0500

    Update precompiler.c

--- experimental/precompiler.c
+++ experimental/precompiler.c
@@ -3,36 +3,19 @@
 #include <malloc.h>
 #include <stdlib.h>
 
-char* RemoveSpaces(char* source)
+char *trimwhitespace(char *str)
 {
-    char* a = source;
-    char* b = source;
-    while(*b != 0)
-    {
-        *a = *b++;
-        if(*a != ' ')
-            a++;
-    }
-    *a = 0;
+    char *end;
+    while(isspace((unsigned char)*str)) str++;
+    if(*str == 0)
+        return str;
+    end = str + strlen(str) - 1;
+    while(end > str && isspace((unsigned char)*end)) end--;
+    end[1] = '\0';
+    return str;
 }
 
-char* readLine() {
-    char c[1000];
-    FILE *fptr;
-    if ((fptr = fopen("index.aml", "r")) == NULL)
-    {
-        printf("Error! opening file");
-        exit(1);
-    }
-    fscanf(fptr,"%[^\n]", c);
-    printf("Data from the file:\n%s", c);
-    fclose(fptr);
-    char *returner = malloc(strlen(c));
-    //strcpy(returner, c);
-    return returner;
-}
-
-char* concat1(const char *s1, const char *s2, const char *s3, const char *s4, const char *s5)
+char* concat1(char *s1, char *s2, char *s3, char *s4, char *s5)
 {
     char *result = malloc(strlen(s1) + strlen(s2) + + strlen(s2) + strlen(s4) + strlen(s5) + strlen(s2) + strlen(s3));
     strcpy(result, s1);
@@ -45,7 +28,7 @@ char* concat1(const char *s1, const char *s2, const char *s3, const char *s4, co
     return result;
 }
 
-char* concat2(const char *s1, const char *s2, const char *s3, const char *s4)
+char* concat2(char *s1, char *s2, char *s3, char *s4)
 {
     char *result = malloc(strlen(s1) + strlen(s2) + + strlen(s3) + strlen(s4));
     strcpy(result, s1);
@@ -55,7 +38,7 @@ char* concat2(const char *s1, const char *s2, const char *s3, const char *s4)
     return result;
 }
 
-char* concat3(const char *s1, const char *s2, const char *s3, const char *s4, const char *s5, const char *s6)
+char* concat3(char *s1, char *s2, char *s3, char *s4, char *s5, char *s6)
 {
     char *result = malloc(strlen(s1) + strlen(s2) + + strlen(s2) + strlen(s4) + strlen(s5) + strlen(s2) + strlen(s3) + strlen(s6));
     strcpy(result, s1);
@@ -70,26 +53,47 @@ char* concat3(const char *s1, const char *s2, const char *s3, const char *s4, co
     return result;
 }
 
-const char * parse() {
-    char line[] = "Loading data";
-    strcpy(line,readLine());
-    printf("%s", line);
-    //"h1 -> class=\"nttile\" -> text";
+char* concat4(char *s1, char *s2, char *s3, char *s4)
+{
+    char *result = malloc(strlen(s1) + strlen(s2) + + strlen(s3) + strlen(s4));
+    strcpy(result, s1);
+    strcat(result, s2);
+    strcat(result, " ");
+    strcat(result, s3);
+    strcat(result, s4);
+    return result;
+}
+
+const char * parse(char * gotten) {
+    char *line = malloc(strlen(gotten));
+    strcpy(line,gotten);
     int i = 0;
-    char *p = strtok(line, " -> ");
+    char delim[] = "->";
+    char *p = strtok(line, delim);
     char *array[3] = {NULL};
     while (p != NULL)
     {
-        array[i++] = p;
-        p = strtok(NULL, " -> ");
+        //printf("%s\n",trimwhitespace(p));
+        array[i++] = trimwhitespace(p);
+        p = strtok(NULL, delim);
     }
-    //printf("%s", RemoveSpaces(array[0]));
     if (strcmp(array[0], "h1") == 0
             || strcmp(array[0], "h2") == 0
             || strcmp(array[0], "h3") == 0
             || strcmp(array[0], "h4") == 0
             || strcmp(array[0], "h5") == 0
-            || strcmp(array[0], "h6") == 0) {
+            || strcmp(array[0], "h6") == 0
+            || strcmp(array[0], "p") == 0
+            || strcmp(array[0], "time") == 0
+            || strcmp(array[0], "a") == 0
+            || strcmp(array[0], "abbr") == 0
+            || strcmp(array[0], "button") == 0
+            || strcmp(array[0], "li") == 0
+            || strcmp(array[0], "small") == 0
+            || strcmp(array[0], "u") == 0
+            || strcmp(array[0], "b") == 0
+            || strcmp(array[0], "s") == 0
+            || strcmp(array[0], "strong") == 0) {
         if (array[2] == NULL) {
             return concat1("<", array[0], ">", array[1], "</");
         } else {
@@ -101,13 +105,53 @@ const char * parse() {
                 return concat3("<", array[0], array[1], ">", array[2], "</");
             }
         }
-    } else if (strcmp(array[0], "div") == 0) {
-        return "div";
+    } else if (strcmp(array[0], "div") == 0
+               || strcmp(array[0], "section") == 0
+               || strcmp(array[0], "article") == 0
+               || strcmp(array[0], "span") == 0
+               || strcmp(array[0], "center") == 0
+               || strcmp(array[0], "header") == 0
+               || strcmp(array[0], "nav") == 0
+               || strcmp(array[0], "main") == 0
+               || strcmp(array[0], "form") == 0
+               || strcmp(array[0], "table") == 0
+               || strcmp(array[0], "th") == 0
+               || strcmp(array[0], "tr") == 0
+               || strcmp(array[0], "td") == 0
+               || strcmp(array[0], "pre") == 0
+               || strcmp(array[0], "code") == 0) {
+        if (array[2] == NULL) {
+            return concat4("<", array[0], array[1], ">");
+        } /*else {
+							if (strcmp(array[2], "") == 0
+									|| strcmp(array[2], ".") == 0
+									|| strcmp(array[2], " ") == 0) {
+									return concat4("<", array[0], array[1], ">", "</");
+							}
+						}*/
     } else {
         return "else";
     };
 }
 
+char* readLine() {
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    fp = fopen("index.aml", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+    while ((read = getline(&line, &len, fp)) != -1) {
+        //printf("Retrieved line of length %zu:\n", read);
+        printf("%s", parse(line));
+    }
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
+}
+
 int main(void) {
-    printf("%s", parse());
+    readLine();
 }
